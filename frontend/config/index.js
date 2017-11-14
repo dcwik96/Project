85f5600b7675e3292554e6cbb157ed899ea8548c
whoamix <mfierek@protonmail.ch>
2017-11-14 14:50:03 +0100

    Edit config/index.js

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -50,7 +50,9 @@ module.exports = {
   
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../target/dist/index.html'),
+     index: path.resolve(__dirname, '../target/dist/index.html'),
+
+    // Paths
     assetsRoot: path.resolve(__dirname, '../target/dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
