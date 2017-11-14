ffac27ad40a056fb2e5dabee9409b28d8be896b5
whoamix <mfierek@protonmail.ch>
2017-11-14 14:16:26 +0100

    Delete package-lock.json

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -51,7 +51,7 @@ module.exports = {
   build: {
     // Template for index.html
     index: path.resolve(__dirname, '../target/dist/index.html'),
-	assetsRoot: path.resolve(__dirname, '../target/dist'),
+    assetsRoot: path.resolve(__dirname, '../target/dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
