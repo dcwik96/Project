8088f795972eef786350ab6db390fe460d6c384a
Mariusz Tkaczyk <m2tkaczyk@gmail.com>
2017-11-17 16:56:25 +0100

    Change Http header and display image

--- backend/src/main/java/pl/iledasz/DTO/PhotoDTO.java
+++ backend/src/main/java/pl/iledasz/DTO/PhotoDTO.java
@@ -3,25 +3,14 @@ package pl.iledasz.DTO;
 
 public class PhotoDTO {
 
-    private Long id;
-
     private byte[] image;
 
     public PhotoDTO(Long id, byte[] image) {
-        this.id = id;
         this.image = image;
     }
 
     public PhotoDTO() {
 
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public byte[] getImage() {
