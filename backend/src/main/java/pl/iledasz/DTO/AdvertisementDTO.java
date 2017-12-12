9a6ed7f7723bd3fe33d0894425422b755f94c041
Mariusz Tkaczyk <m2tkaczyk@gmail.com>
2017-12-12 21:19:49 +0100

    Initialize time and data for advert.

--- backend/src/main/java/pl/iledasz/DTO/AdvertisementDTO.java
+++ backend/src/main/java/pl/iledasz/DTO/AdvertisementDTO.java
@@ -5,8 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import pl.iledasz.entities.AdvertPhoto;
 
+import java.time.OffsetDateTime;
 import java.util.List;
 
 @Getter
@@ -18,6 +18,9 @@ public class AdvertisementDTO {
     private Long id;
     private String title;
     private String description;
+    private OffsetDateTime startDate;
+    private Long duration;
+    private OffsetDateTime endDate;
     private List<AdvertPhotoDTO> photos;
 
 }
--- backend/src/main/java/pl/iledasz/entities/Advertisement.java
+++ backend/src/main/java/pl/iledasz/entities/Advertisement.java
@@ -6,10 +6,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import javax.persistence.*;
 import org.hibernate.annotations.Cascade;
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
 
+import java.time.OffsetDateTime;
 import java.util.List;
+import org.hibernate.annotations.Type;
 
 @Entity
 @Table(name = "advert")
@@ -33,15 +33,32 @@ public class Advertisement {
     @JoinColumn(name = "app_user_id")
     private AppUser appUser;
 
+    @Column(name = "start_date", nullable = false)
+    @Type(type = "org.hibernate.type.OffsetDateTimeType")
+    private OffsetDateTime startDate;
+
+    @Column(name = "duration", nullable = false)
+    private Long duration;
+
+    @Column(name = "end_date", nullable = false)
+    @Type(type = "org.hibernate.type.OffsetDateTimeType")
+    private OffsetDateTime endDate;
+
 
     @OneToMany (mappedBy = "advertisement")
     @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
     private List<AdvertPhoto> photos;
 
-    public Advertisement(String title, String description, AppUser appUser, List<AdvertPhoto> photos) {
+    public Advertisement(String title, String description, AppUser appUser, OffsetDateTime startDate,
+                         Long duration, OffsetDateTime endDate, List<AdvertPhoto> photos) {
         this.title = title;
         this.description = description;
         this.appUser = appUser;
+        this.startDate = startDate;
+        this.duration = duration;
+        this.endDate = endDate;
         this.photos = photos;
+
+
     }
 }
