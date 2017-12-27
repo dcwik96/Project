094130c0a98b241c390e29fa4211184c5af83e16
Mariusz Tkaczyk <m2tkaczyk@gmail.com>
2017-12-27 21:27:02 +0100

    Return username after login. Add static folder to gitgnore

--- backend/src/main/java/pl/iledasz/Security/CustomAuthenticationSuccessHandler.java
+++ backend/src/main/java/pl/iledasz/Security/CustomAuthenticationSuccessHandler.java
@@ -3,6 +3,9 @@ package pl.iledasz.Security;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -15,15 +18,27 @@ import java.io.IOException;
 @Component
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+
 
         HttpSession session = httpServletRequest.getSession();
         User authUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         session.setAttribute("username", authUser.getUsername());
         session.setAttribute("authorities", authentication.getAuthorities());
+        clearAuthenticationAttributes(httpServletRequest);
 
-        httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+        redirectStrategy.sendRedirect(httpServletRequest, httpServletResponse,"/aboutMe" );
 
+    }
+
+    protected void clearAuthenticationAttributes(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return;
+        }
+        session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
     }
 }
--- backend/src/main/java/pl/iledasz/controllers/HomeController.java
+++ backend/src/main/java/pl/iledasz/controllers/HomeController.java
@@ -2,6 +2,7 @@ package pl.iledasz.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import pl.iledasz.DTO.AdvertPhotoDTO;
@@ -11,7 +12,10 @@ import pl.iledasz.service.AdvertPhotoService;
 import pl.iledasz.service.AppUserService;
 
 import javax.servlet.http.HttpSession;
+import java.security.Principal;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class HomeController {
@@ -54,6 +58,13 @@ public class HomeController {
     @RequestMapping(value = "/users")
     public List<AppUser> getUsers() {
         return appUserService.getUsers();
+    }
+
+    @RequestMapping(value = "/aboutMe", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Map aboutMe(Principal principal) {
+        return Collections.singletonMap("username", principal.getName());
+
     }
 
     @GetMapping(value = "/logout")
