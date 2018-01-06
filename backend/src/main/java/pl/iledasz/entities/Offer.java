1f79ae8d770f22becc9b765832b035cda1f4de05
mfierek <mfierek@protonmail.ch>
2018-01-06 13:32:09 +0100

    Revert "Revert "Offer implementation devel""

---
+++ backend/src/main/java/pl/iledasz/entities/Offer.java
@@ -0,0 +1,38 @@
+package pl.iledasz.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "offer")
+public class Offer {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "offer", nullable = false)
+    private BigDecimal offer;
+
+    @OneToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "app_user_id")
+    private AppUser appUser;
+
+    @OneToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "advert_id")
+    private Advertisement advertisement;
+
+    public Offer(BigDecimal offer, AppUser appUser, Advertisement advertisement) {
+        this.offer = offer;
+        this.appUser = appUser;
+        this.advertisement = advertisement;
+    }
+}
