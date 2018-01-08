300d658efe5a1a33dbc5c3b1dcdf29ddbf5a4c4b
whoamix <mfierek@protonmail.ch>
2018-01-08 19:42:51 +0100

    Working on advert view

--- frontend/src/components/ItemView.vue
+++ frontend/src/components/ItemView.vue
@@ -1,26 +1,29 @@
-
-
 <template>
   <div>
     <div class="panel panel-primary text-center">
-
       <div class="panel-heading"><h3>{{advert.title}}</h3></div>
+      <br>
+      <div class="alert alert-success" v-if="offerAccepted">{{message}}</div>
+      <div class="alert alert-danger" v-if="someProblem">{{message}}</div>
       <br><br>
-
-
     <img v-img:test :src="'http://localhost:8080/api/photos/' + advert.photos[0].id" style="width: 300px;">
-
       <div class="panel-body"><h6>{{advert.description}}</h6></div>
-
     <br>
-
-     <p><a href="/" class="btn btn-success" role="button">Ile dasz?</a> <router-link to= "/" tag="button" class="btn btn-default" exact>Powrót</router-link> </p>
-
-
+     <p>
+       <button class="btn btn-success" @click="makeOffer">Ile dasz?</button>
+       <input type="text" style="width: 100px" ref="priceInput">
+       <router-link to= "/" tag="button" class="btn btn-default" exact>Powrót</router-link>
+     </p>
+      <div class="small-12 columns"v-if="offers.length > 0 && isValidUser">
+        <br>
+        <h2>Złożone ofery</h2>
+        <ul class="list-group" >
+          <li class="list-group-item" v-for="offer in offers">
+            {{ offer.price }}
+          </li>
+        </ul>
+      </div>
   </div>
-
-
-
   </div>
 </template>
 
@@ -34,7 +37,11 @@ Vue.use(VueImg);
     data () {
       return {
         id: '',
-        advert: {}
+        advert: {},
+        offerAccepted: false,
+        message: '',
+        someProblem: false,
+        offers:[{price: 100}, {price:1200}, {price:1300}]
       }
 
     },
@@ -52,6 +59,22 @@ Vue.use(VueImg);
             return
           }
         }
+      },
+      makeOffer() {
+        var formData = new FormData();
+        formData.append('offer', this.$refs.priceInput.value)
+        this.$http.post('http://localhost:8080/api/advert/'+this.advert.id+'/newOffer',formData)
+          .then(
+            response => {
+              this.offerAccepted = true;
+              this.message = 'Twoja oferta została dodana.'
+            console.log(response)
+          },
+            response => {
+              this.someProblem = true
+              this.message = 'Wystąpił problem przy składaniu oferty'
+              console.log(response)
+          })
       }
     }
   }
