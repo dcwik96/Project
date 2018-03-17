11cb51936ae393d8168bbae53a0dafee92657bbf
Mariusz Tkaczyk <m2tkaczyk@gmail.com>
2018-03-17 22:56:03 +0100

    Add One photo endpoint implementation

--- backend/src/main/java/pl/iledasz/controllers/PhotoController.java
+++ backend/src/main/java/pl/iledasz/controllers/PhotoController.java
@@ -5,13 +5,21 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import pl.iledasz.DTO.PhotoDTO;
+import pl.iledasz.entities.Advertisement;
 import pl.iledasz.entities.Photo;
+import pl.iledasz.repository.AdvertisementRepository;
+import pl.iledasz.service.AdvertisementService;
 import pl.iledasz.service.PhotoService;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -19,6 +27,11 @@ public class PhotoController {
 
     @Autowired
     private PhotoService photoService;
+
+    @Autowired
+    private AdvertisementRepository advertisementRepository;
+
+    private  static String default_image_desc = "desc";
 
     @RequestMapping(value = "api/photos")
     public List<PhotoDTO> getPhotos() {
@@ -34,5 +47,16 @@ public class PhotoController {
         headers.setContentType(MediaType.IMAGE_JPEG);
         headers.setContentLength(photo.getImage().length);
         return new ResponseEntity<>(photo.getImage(), headers, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "api/{advertId}/sendNudes")
+    public void addPhotoToAdvert(@PathVariable("advertId") Long advertID,Principal principal, @ModelAttribute MultipartFile image, HttpServletResponse httpServletResponse){
+        httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+
+        try {
+            photoService.addPhotoToAdvert(advertID, default_image_desc, image.getBytes(), principal);
+        }catch (IOException | SecurityException e) {
+            httpServletResponse.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        }
     }
 }
--- backend/src/main/java/pl/iledasz/service/PhotoService.java
+++ backend/src/main/java/pl/iledasz/service/PhotoService.java
@@ -4,10 +4,17 @@ package pl.iledasz.service;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import pl.iledasz.DTO.PhotoDTO;
+import pl.iledasz.entities.AdvertPhoto;
+import pl.iledasz.entities.Advertisement;
 import pl.iledasz.entities.Photo;
+import pl.iledasz.repository.AdvertPhotoRepository;
+import pl.iledasz.repository.AdvertisementRepository;
 import pl.iledasz.repository.PhotoRepository;
 
+import java.io.IOException;
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +23,12 @@ public class PhotoService {
 
     @Autowired
     private PhotoRepository photoRepository;
+
+    @Autowired
+    private AdvertPhotoRepository advertPhotoRepository;
+
+    @Autowired
+    private AdvertisementRepository advertisementRepository;
 
     public List<PhotoDTO> list() {
 
@@ -33,5 +46,20 @@ public class PhotoService {
 
     public Photo findPhotoById(Long id) {
         return photoRepository.findByAdvertphoto_Id(id);
+    }
+
+    @Transactional
+    public void addPhotoToAdvert(Long advertId , String desc, byte[] image, Principal principal) throws IOException {
+
+        Advertisement advertisement = advertisementRepository.findAdvertisementsByAppUser_LoginAndId(principal.getName(), advertId);
+
+        if( advertisement == null)
+            throw new SecurityException("Advert don't exist");
+
+        AdvertPhoto advertPhoto = new AdvertPhoto(advertisement, desc);
+        advertPhotoRepository.save(advertPhoto);
+
+        Photo photo = new Photo( image, advertPhoto);
+        photoRepository.save(photo);
     }
 }
